e2626cda0bdbcf50c8262835b3100dd25eb983d2
Emerson Leao Junior <emerson.leaojr@gmail.com>
2022-04-19 14:37:18 -0300

    questions 6-12

--- scripts/communities-crime.py
+++ scripts/communities-crime.py
@@ -12,6 +12,18 @@ def violentCrimes_float(df):
 	return df.withColumn('ViolentCrimesPerPop',
 		F.col('ViolentCrimesPerPop').cast(DoubleType()))
 
+def policeBudget(df):
+	return (df.withColumn('PolicOperBudg',
+				F.col('PolicOperBudg').cast(DoubleType())))
+
+def whitePolicePct_float(df):
+	return df.withColumn('PctPolicWhite',
+		F.col('PctPolicWhite').cast(DoubleType()))
+
+def medianFamily_float(df):
+	return (df.withColumn('medFamInc',
+				F.col('medFamInc').cast(DoubleType())))
+
 def violentCrimes(df):
 	return (df.withColumn('violent_crimes',
 		F.expr('population * ViolentCrimesPerPop')))
@@ -19,6 +31,35 @@ def violentCrimes(df):
 def blackPop(df):
 	return (df.withColumn('black_population',
 						F.expr('population * racepctblack')))
+
+def youngPct_float(df):
+	return (df.withColumn('agePct12t21',
+				F.col('agePct12t21').cast(DoubleType())))
+
+def youngPopulation(df):
+	return (df.withColumn('young_population',
+				F.expr('population * agePct12t21')))
+
+def whitePolice(df):
+	return (df.withColumn('white_police',
+				F.expr('population * PctPolicWhite')))
+
+def blackPct_float(df):
+	return (df.withColumn("racepctblack",
+				F.col('racepctblack').cast(DoubleType())))
+
+def whitePct_float(df):
+	return ((df.withColumn("racePctWhite",
+				F.col('racePctWhite').cast(DoubleType()))))
+
+def asianPct_float(df):
+	return (df.withColumn("racePctAsian",
+				F.col('racePctAsian').cast(DoubleType())))
+
+def hispPct_float(df):
+	return (df.withColumn("racePctHisp",
+				F.col('racePctHisp').cast(DoubleType())))
+
 
 ### Questions
 def communityPoliceBudget(df):
@@ -49,6 +90,44 @@ def highestPercentWage(df):
 		.sort('salary', ascending = False)
 		.show(1))
 
+def highestYoungPopCommunity(df):
+	(df.select('communityname', 'young_population')
+		.sort('young_population', ascending = False)
+		.show(1))
+
+def correlationPoliceBudgetxViolentCrimes(df):
+	corr_polBudg_vioCrim = round(df.corr('PolicOperBudg', 'violent_crimes', 'pearson'), 2)
+	print(f"Correlation between Police Budget and Violent Crimes: {corr_polBudg_vioCrim}.")
+
+def correlationWhitePolicexPoliceBudget(df):
+	corr_whitePol_polBudg = round(df.corr('white_police', 'PolicOperBudg', 'pearson'), 2)
+	print(f"Correlation between white police and police budget: {corr_whitePol_polBudg}.")
+
+def correlationPoliceBudgetPopulation(df):
+	corr_polBudg_population = round(df.corr('population', 'PolicOperBudg', 'pearson'), 2)
+	print(f"Correlation between police budget and population: {corr_polBudg_population}.")
+
+def correlationPopulationViolentCrimes(df):
+	corr_pop_vioCrim = round(df.corr('population', 'violent_crimes', 'pearson'), 2)
+	print(f'Correlation between population and violent crimes: {corr_pop_vioCrim}.')
+
+def correlationMedianFamilyIncomeViolentCrimes(df):
+	corr_medFamInc_vioCrim = round(df.corr('medFamInc', 'violent_crimes', 'pearson'), 2)
+	print(f"Correlation between median family income and violent crimes: {corr_medFamInc_vioCrim}.")
+
+def predominantRaceViolentCrimes(df):
+	(df.select('communityname', 
+				'racepctblack', 
+				'racePctWhite', 
+				'racePctAsian', 
+				'racePctHisp', 
+				F.greatest('racepctblack', 
+							'racePctWhite', 
+							'racePctAsian', 
+							'racePctHisp').alias('predominant_race_perc'))
+		.sort('violent_crimes', ascending = False)
+		.show(10))
+
 if __name__ == "__main__":
 	sc = SparkContext()
 	spark = (SparkSession.builder.appName("Aceleração PySpark - Capgemini [Communities & Crime]"))
@@ -58,15 +137,34 @@ if __name__ == "__main__":
 		          .option("header", "true")
 		          #.schema(schema_communities_crime)
 		          .load("/home/spark/capgemini-aceleracao-pyspark/data/communities-crime/communities-crime.csv"))
-	#print(df.show())
+	# print(df.show())
+
 
 	df = population_float(df)
 	df = violentCrimes_float(df)
+	df = policeBudget(df)
+	df = youngPct_float(df)
 	df = violentCrimes(df)
 	df = blackPop(df)
+	df = youngPopulation(df)
+	df = policeBudget(df)
+	df = whitePolicePct_float(df)
+	df = whitePolice(df)
+	df = medianFamily_float(df)
+	df = blackPct_float(df)
+	df = whitePct_float(df)
+	df = asianPct_float(df)
+	df = hispPct_float(df)
 
-	communityPoliceBudget(df) #Pergunta 1
+	# communityPoliceBudget(df) #Pergunta 1
 	# violentCrimesCommunity(df) #Pergunta 2
-	# populationCommunity(df)
-	# highestBlackPop(df)
-	# highestPercentWage(df)
+	# populationCommunity(df) #Pergunta 3
+	# highestBlackPop(df) #Pergunta 4
+	# highestPercentWage(df) #Pergunta 5
+	# highestYoungPopCommunity(df) #Pergunta 6
+	# correlationPoliceBudgetxViolentCrimes(df) #Pergunta 7
+	# correlationWhitePolicexPoliceBudget(df) #Pergunta 8
+	# correlationPoliceBudgetPopulation(df) #Pergunta 9
+	# correlationPopulationViolentCrimes(df) #Pergunta 10
+	# correlationMedianFamilyIncomeViolentCrimes(df) #Pergunta 11
+	predominantRaceViolentCrimes(df)
